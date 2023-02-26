ce1bec6679263c1fc0749d824dee174205a88e4a
Alexandre Chagas <afschagas@gmail.com>
2023-02-26 17:06:04 -0300

    Alteração do favicon, logo e animate para ping

--- src/components/Nav/NavLogo.tsx
+++ src/components/Nav/NavLogo.tsx
@@ -1,10 +1,10 @@
-import Logo from '../../assets/image/teste.png';
+import Logo from '../../assets/image/ac-icon.png';
 
 export function NavLogo() {
   return (
     <div>
 
-      <img className="h-8 animate-bounce" src={Logo} alt="Logo" />
+      <img className="h-8 animate-ping" src={Logo} alt="Logo" />
 
 
     </div>
