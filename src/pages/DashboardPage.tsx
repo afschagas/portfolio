c8c930528a0db098bfa90a0e32f49f2c2f98edd6
Alexandre Chagas <afschagas@gmail.com>
2023-02-28 18:15:41 -0300

    Novas funcionalidades

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,86 +1,43 @@
-//import { Nav } from 'components';
-
-//import { Button } from 'components';
-
 import { Link } from 'react-router-dom';
 
-import Ellipse from '../assets/image/Ellipse.png';
-import JsLogo from '../assets/image/loading.png';
-import WaveTres from '../assets/image/wave3.png';
-import Wave from '../assets/image/wave.png';
-
 export function DashboardPage() {
+
   return (
+    <div className="absolute top-0 w-full h-full bg-center bg-cover"
+      style={{
+        backgroundImage: "url('https://t4.ftcdn.net/jpg/01/69/56/13/360_F_169561381_OyknorO0aROyTrFCU5LGL8lPob140o6F.jpg')"
+      }}>
+      <span id="blackOverlay" className="w-full h-full absolute opacity-75 bg-black"></span>
 
-    <section className="bg-[#232425] h-screen flex px-[15%] py-[7%]">
+      <div className="flex px-[12%] py-[20%]">
+        <div className="">
 
-      <div className="space-y-8 md:space-y-0 md:grid md:grid-cols-2 ">
-        <img
-          src={Ellipse}
-          className="z-20 animate-ping w-6 absolute left-24 top-56"
-          alt="Imagem circulo"
-        />
+          <div className="flex-1 pt-10">
+            <div className="absolute">
 
-        <img
-          src={Ellipse}
-          className="z-20 animate-ping w-6 absolute right-96 top-36"
-          alt="Imagem circulo"
-        />
-
-        <img
-          src={Ellipse}
-          className="z-20 animate-ping w-6 absolute left-64 bottom-24"
-          alt="Imagem circulo"
-        />
-
-        <img
-          src={WaveTres}
-          className="absolute bottom-2 right-36 w-96"
-          alt="Imagem Onda"
-        />
-
-        <img
-          src={Wave}
-          className="absolute bottom-0 right-0 w-2/3"
-          alt="Imagem Onda"
-        />
-
-        <img
-          src={Ellipse}
-          className="z-20 animate-ping w-6 absolute right-40 top-64"
-          alt="Imagem circulo"
-        />
-
-        <div className="flex-1">
-          <img
-            src={JsLogo} className="w-9/12"
-            alt="Imagem a lua e estrela com prédios"
-          />
-        </div>
-
-
-
-        <div className="flex-1 pt-7">
-          <div className="absolute">
-            <h1 className="text-3xl leading-normal py-6">
-              Seja Bem Vindo <br />
-              ao meu<br />
-              Portfolio! <br />
-
-            </h1>
-            <Link to="/sobre">
-              <button className="bg-teal-600 px-7 rounded-full -tracking-wide py-3 text-xs hover:scale-110 duration-300">
-                VAMOS COMEÇAR
-              </button>
-            </Link>
+              <h1 className="text-white font-semibold text-5xl">
+                Seja Bem Vindo ao meu Portfolio!
+              </h1>
+              <p className="mt-4 text-lg text-gray-300">
+                Começar projetos novos sempre gera uma ansiedade,não sabemos o que vem pela frente e não conhecemos as pessoas com quem vamos nos relacionar.<br />
+                <br />
+                Por isso, estou compartilhando um pouco da minha história e meu trabalho.
+              </p>
+              <br />
+              <Link to="/sobre">
+                <button className="bg-teal-600 px-7 rounded-full -tracking-wide py-3 text-xs hover:scale-110 duration-300">
+                  VAMOS COMEÇAR
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
 
-    </section>
-
-
+    </div>
   )
-
 }
+
+
+
 
--- src/pages/Sobre.tsx
+++ src/pages/Sobre.tsx
@@ -7,6 +7,7 @@ import IconJavaScript from '../assets/image/icons-javascript.png';
 import IconReact from '../assets/image/icons-react.png';
 import IconGitHub from '../assets/image/icons-git.png';
 import IconTailwind from '../assets/image/icons-tailwind-css.png';
+import IconTypeScript from '../assets/image/icons-typescript.png';
 
 import { Button } from 'components';
 
@@ -20,12 +21,17 @@ export function Sobre() {
 
         <div className="space-y-8 md:space-y-0 md:grid md:grid-cols-2 py-[12%]">
 
-          <figure>
-            <img className="shadow-2xl rounded-lg transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0" src="https://github.com/afschagas.png" alt="Segurando uma caneca" />
-            <figcaption className="mt-2 text-sm text-left text-gray-500 dark:text-gray-400">Sem café, eu não vivo...</figcaption>
-          </figure>
+          <div className="flex flex-wrap justify-center">
+            <div className="w-6/12 sm:w-4/12 px-4">
+              <img src="https://github.com/afschagas.png" className="shadow rounded-full max-w-full h-auto align-middle border-none transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0" alt="Segurando uma caneca de café" />
+            </div>
+          </div>
 
           <div className="md:flex md:flex-col md:justify-center">
+            <h1 className="text-white font-semibold text-2xl">
+              Olá, Sou Alexandre Chagas!
+            </h1>
+            <br />
             <h2 className="text-gray-400 text-2xl md:text-4xl lg:text-6xl font-bold mb-4">
               Desenvolvedor Frontend
             </h2>
@@ -36,6 +42,11 @@ export function Sobre() {
             <br />
             <hr className="text-gray-500" />
             <br />
+            <h3 className="text-white font-semibold text-2xl">
+              Tech Stack
+            </h3>
+
+            <p></p>
             <div className="container flex max-w-screen-lg mx-auto">
               <div className="flex justify-center gap-2">
                 <i className="" title="HTML">
@@ -52,6 +63,10 @@ export function Sobre() {
 
                 <i className="" title="React">
                   <img src={IconReact} alt="Ícone da linguagem React" />
+                </i>
+
+                <i className="" title="TypeScript">
+                  <img src={IconTypeScript} alt="Ícone da linguagem TypeScript" />
                 </i>
 
                 <i className="" title="GitHub">
@@ -82,3 +97,4 @@ export function Sobre() {
 }
 
 
+// <figcaption className="mt-2 text-sm text-end text-gray-500 dark:text-gray-400">Sem café, eu não vivo...</figcaption>
